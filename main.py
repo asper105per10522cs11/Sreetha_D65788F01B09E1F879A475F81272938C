aea7f67d587987fbae4cf44a2176a973a6a13b03
asper105per10522cs11 <144432728+asper105per10522cs11@users.noreply.github.com>
2023-09-25 20:16:37 +0530

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-# fatctorial function
-def factorial(number):
-  if number==0 or number==1:
-    return 1
-  else :
-   return number*factorial(number-1)
-result=factorial(5) 
-print("the given number's factorial  is ",result)
